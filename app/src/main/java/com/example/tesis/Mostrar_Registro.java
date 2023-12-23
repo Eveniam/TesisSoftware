b2a13d3f2112da0d12908581f0479c08236ddd7d
Eveniam <102640726+Eveniam@users.noreply.github.com>
2023-12-23 14:05:40 -0500

    main

--- app/src/main/java/com/example/tesis/Mostrar_Registro.java
+++ app/src/main/java/com/example/tesis/Mostrar_Registro.java
@@ -1,14 +1,30 @@
 package com.example.tesis;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.tesis.Adapter.UserAdapter;
+import com.example.tesis.Entities.User;
+import com.example.tesis.Utils.RetrofitBuilder;
+
+import retrofit2.Retrofit;
+
 public class Mostrar_Registro extends AppCompatActivity {
+
+    RecyclerView rvUser;
+    Retrofit mRetro;
+
+    User mUser = new User();
+
+    UserAdapter mAdapter = new UserAdapter(mUser);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostrar_registro);
+
+
     }
 }
